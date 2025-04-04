2b1d59069b6d1443302a017b7ae4018c5031de08
James Stevam <jstevam@barbosaebarbosa.com>
2025-04-04 15:44:40 -0300

    Criação das paginas

--- src/utils/actions/get.data.ts
+++ src/utils/actions/get.data.ts
@@ -1,15 +1,32 @@
-export async function getDataHome(){
+export async function getDataHome() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/objects/67ba183c3b8d8645536f26df?pretty=true&read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata`,
+      { next: { revalidate: 120 } }
+    );
 
-    try {
-        const res= await fetch(`${process.env.NEXT_PUBLIC_API_URL}/objects/67ba183c3b8d8645536f26df?pretty=true&read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata`)
-
-        if (!res.ok){
-            throw new Error("Failed to fetch data falha")
-        }
-         
-         return res.json();
-
-    } catch(err) {
-        throw new Error("Failed to fetch data falha")
+    if (!res.ok) {
+      throw new Error("Failed to fetch data falha");
     }
+
+    return res.json();
+  } catch (err) {
+    throw new Error("Failed to fetch data falha");
+  }
+}
+
+export async function getSubMenu() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}objects?pretty=true&query=%7B%22type%22:%22pages%22%7D&limit=10&skip=0&read_key=${process.env.READ_KEY}&depth=1&props=slug,title`,
+      { next: { revalidate: 120 } }
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch menu data");
+    }
+    return res.json();
+  } catch (err) {
+    throw new Error("Failed to fetch menu data");
+  }
 }
---
+++ src/utils/menu.types.ts
@@ -0,0 +1,8 @@
+export interface MenuProps {
+  objects: ItemMenuProps[];
+}
+
+interface ItemMenuProps {
+  title: string;
+  slug: string;
+}
