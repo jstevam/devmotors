2e6e573a7e0a38ce189e7484fa7ca39b7effb573
James Stevam <jstevam@barbosaebarbosa.com>
2025-04-08 12:49:00 -0300

    Correção do arquivo get.data

--- src/utils/actions/get.data.ts
+++ src/utils/actions/get.data.ts
@@ -18,7 +18,7 @@ export async function getDataHome() {
 export async function getSubMenu() {
   try {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}objects?pretty=true&query=%7B%22type%22:%22pages%22%7D&limit=10&skip=0&read_key=${process.env.READ_KEY}&depth=1&props=slug,title`,
+      `${process.env.NEXT_PUBLIC_API_URL}/objects?pretty=true&query=%7B%22type%22:%22pages%22%7D&limit=10&skip=0&read_key=${process.env.READ_KEY}&depth=1&props=slug,title`,
       { next: { revalidate: 120 } }
     );
 
