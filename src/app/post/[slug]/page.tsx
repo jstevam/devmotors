c6f51bb3e0eacca98a1b1e27795727b30bac53fc
James Stevam <jstevam@gmail.com>
2025-04-09 21:28:53 -0300

    Criação do post.type

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,12 +1,20 @@
 import styles from "./styles.slug.module.scss";
+import { getItemBySlug } from "@/utils/actions/get.data";
+import { PostProps } from "@/utils/post.type";
 
-export default function Page() {
+
+
+export default async function Page({ params: { slug } }: {
+    params: { slug: string }
+}) {
+
+    const { objects }: PostProps = await getItemBySlug(slug);
+    console.log(JSON.stringify(objects, null, 2));
 
     return (
         <div>
-            <h1>Page</h1>
+            <h1>Page {slug}</h1>
         </div>
     );
 
 };
-
--- src/utils/actions/get.data.ts
+++ src/utils/actions/get.data.ts
@@ -30,3 +30,30 @@ export async function getSubMenu() {
     throw new Error("Failed to fetch menu data");
   }
 }
+
+export async function getItemBySlug(itemSlug: string) {
+  const baseUrl = `${process.env.NEXT_PUBLIC_API_URL}/objects`;
+
+  // Definindo o objeto de consulta pelo Slug
+  const queryParams = new URLSearchParams({
+    query: JSON.stringify({
+      slug: itemSlug,
+    }),
+    props: "slug,title,content,metadata",
+    read_key: process.env.READ_KEY as string,
+  });
+
+  const url = `${baseUrl}?${queryParams.toString()}`;
+
+  try {
+    const res = await fetch(url, { next: { revalidate: 120 } });
+
+    if (!res.ok) {
+      throw new Error("Failed get item by slug");
+    }
+
+    return res.json();
+  } catch (err) {
+    throw new Error("Failed get item by slug");
+  }
+}
